14916d1a3e047899f615b2086461bec2d42a5bfb
1998kuratayumi <kurata.yumi.infratop@gmail.com>
2021-11-11 07:04:17 +0000

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,30 @@
+Rails.application.routes.draw do
+  get 'message/index'
+  get 'message/create'
+  get 'conversations/index'
+  get 'conversations/create'
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root :to => "homes#top"
+  get "home/about" => "homes#about"
+
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
+
+  resources :users, only: [:index, :show, :edit, :update] do
+  	resource :relationships, only: [:create, :destroy]
+  	get 'followings' => 'relationships#followings', as: 'followings'
+  	get 'followers' => 'relationships#followers', as: 'followers'
+  end
+  
+  resources :messages, only: [:create, :show]
+  resources :groups do    
+    get "join" => "groups#join"
+    get "new/mail" => "groups#new_mail"
+    get "send/mail" => "groups#send_mail"
+  end
+
+end
